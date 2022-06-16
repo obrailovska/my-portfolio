6a0a396608c907079c7c5aa33a79aaff8a3c029e
obrailovska <olabrrai@icloud.com>
2022-06-16 18:49:06 -0400

    added floating navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,53 @@
 import React from "react";
+import { useState } from "react";
+import "./navbar.css";
+import { RiHome3Line } from "react-icons/ri";
+import { BiUser } from "react-icons/bi";
+import { AiOutlineProject } from "react-icons/ai";
+import { RiServiceLine } from "react-icons/ri";
+import { AiOutlineMail } from "react-icons/ai";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  const [activeNav, setActiveNav] = useState("#");
+  return (
+    <nav>
+      <a
+        href="#"
+        onClick={() => setActiveNav("#")}
+        className={activeNav === "#" ? "active" : ""}
+      >
+        <RiHome3Line />
+      </a>
+      <a
+        href="#about"
+        onClick={() => setActiveNav("#about")}
+        className={activeNav === "#about" ? "active" : ""}
+      >
+        <BiUser />
+      </a>
+      <a
+        href="#projects"
+        onClick={() => setActiveNav("#projects")}
+        className={activeNav === "#projects" ? "active" : ""}
+      >
+        <AiOutlineProject />
+      </a>
+      <a
+        href="#services"
+        onClick={() => setActiveNav("#services")}
+        className={activeNav === "#services" ? "active" : ""}
+      >
+        <RiServiceLine />
+      </a>
+      <a
+        href="#contact"
+        onClick={() => setActiveNav("#contact")}
+        className={activeNav === "#contact" ? "active" : ""}
+      >
+        <AiOutlineMail />
+      </a>
+    </nav>
+  );
 };
 
 export default NavBar;
