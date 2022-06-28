02b9499fa3c4ea2dbe544c5087dd79b754e42ec6
obrailovska <olabrrai@icloud.com>
2022-06-28 17:19:18 -0400

    added projects section

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,10 +1,83 @@
-import React from 'react'
-import './projects.css'
-
+import React from "react";
+import "./projects.css";
+import Airport from "../../assets/projects/airport-info-search.png";
+import Budget from "../../assets/projects/budget-tracker.jpg";
+import IMS from "../../assets/projects/ims.jpg";
+import PetStore from "../../assets/projects/petstore.png";
+import Password from "../../assets/projects/password.png";
+import RunBuddy from "../../assets/projects/run-buddy.jpg";
+const data = [
+  {
+    id: 1,
+    image: Budget,
+    title: "Budget-Tracker",
+    github: "https://github.com/obrailovska/Budget-Tracker",
+    demo: "https://protected-peak-09794.herokuapp.com/",
+  },
+  {
+    id: 2,
+    image: IMS,
+    title: "Inventrory Management System",
+    github: "https://github.com/obrailovska/IMS",
+    demo: "https://ims-inventory-s.herokuapp.com/",
+  },
+  {
+    id: 3,
+    image: Airport,
+    title: "Flight Search",
+    github: "https://github.com/obrailovska/Flight-Search",
+    demo: "https://user-images.githubusercontent.com/95880256/153761117-8ce99a2f-8d7c-4b51-a0e9-2d2529df7872.gif",
+  },
+  {
+    id: 4,
+    image: PetStore,
+    title: "ProPet - Pet Store",
+    github: "https://github.com/obrailovska/pet-supplies-store",
+    demo: "https://afternoon-spire-43659.herokuapp.com/",
+  },
+  {
+    id: 5,
+    image: Password,
+    title: "Password Generator",
+    github: "https://github.com/obrailovska/password-generator",
+    demo: "https://obrailovska.github.io/password-generator/index.html",
+  },
+  {
+    id: 6,
+    image: RunBuddy,
+    title: "Run Buddy",
+    github: "https://github.com/obrailovska/run-buddy",
+    demo: "https://obrailovska.github.io/run-buddy/",
+  },
+];
 const Projects = () => {
   return (
-    <section id="projects">Projects</section >
-  )
-}
+    <section id="projects">
+      <h5>Recent Projects</h5>
+      <h2>Portfolio</h2>
 
-export default Projects
+      <div className="container portfolio-container">
+        {data.map(({ id, image, title, github, demo }) => {
+          return (
+            <article key={id} className="portfolio-item">
+              <div className="portfolio-item-img">
+                <img src={image} alt={title}></img>
+              </div>
+              <h3>{title}</h3>
+              <div className="portfolio-item-cta">
+                <a href={github} className="btn" target="_blank">
+                  GitHub
+                </a>
+                <a href={demo} className="btn btn-primary" target="_blank">
+                  Live Demo
+                </a>
+              </div>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Projects;
