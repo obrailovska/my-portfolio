f1309ee0504e76e66248294a35524114e9b427a5
obrailovska <olabrrai@icloud.com>
2022-06-29 15:40:06 -0400

    added footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import "./footer.css";
+import { AiOutlineLinkedin } from "react-icons/ai";
+import { AiFillGithub } from "react-icons/ai";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <a href="#" className="footer-logo">
+        Olha Brailovska
+      </a>
+
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+      <div className="footer-socials">
+        <a href="https://github.com/obrailovska">
+          <AiOutlineLinkedin />
+        </a>
+        <a href="https://www.linkedin.com/in/olha-brailovska-24a868226/">
+          <AiFillGithub />
+        </a>
+      </div>
+
+      <div className="footer-copyright">
+        <small>&copy; Olha Brailovska. All rights reserved. </small>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
